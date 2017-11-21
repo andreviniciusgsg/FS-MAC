9cf181b14a8eec2cd2272ce9994b365e13af0e95
andre.gomes@dcc.ufmg.br <andre.gomes@dcc.ufmg.br>
2017-11-21 17:23:16 -0200

    add snr

--- gr-fsmac/examples/transceiver_coord.py
+++ gr-fsmac/examples/transceiver_coord.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: IEEE 802.15.4 Transceiver using OQPSK PHY
-# Generated: Tue Nov 21 17:00:01 2017
+# Generated: Tue Nov 21 17:17:49 2017
 ##################################################
 
 if __name__ == '__main__':
