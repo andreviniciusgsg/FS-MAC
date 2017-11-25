4f50b7a53a7f9cb02c0a975989b5333bb0674e84
andre.gomes@dcc.ufmg.br <andre.gomes@dcc.ufmg.br>
2017-11-25 10:08:02 -0200

    remove build

--- gr-fsmac/build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gnuradio/FS-MACplus/gr-fsmac/apps
-# Build directory: /home/gnuradio/FS-MACplus/gr-fsmac/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gr-fsmac/build/swig/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gnuradio/FS-MACplus/gr-fsmac/swig
-# Build directory: /home/gnuradio/FS-MACplus/gr-fsmac/build/swig
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
